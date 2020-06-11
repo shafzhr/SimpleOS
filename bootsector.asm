57c7198992dea5b9e3cd6beafb8a9ab3b17d4c74
Shachaf Zohar <shafzhr@gmail.com>
2020-06-11 21:05:12 +0300

    print function

--- bootsector.asm
+++ bootsector.asm
@@ -1,6 +1,21 @@
+[org 0x7C00] ; Offset of the boot sector
 start:
-inf_loop:
-    jmp inf_loop
+
+push hello_world
+call print
+call new_line
+
+push welcome
+call print
+
+call new_line
+
+jmp $
+
+%include "bootsector_print.asm"
+
+hello_world db "HELLO!", 0
+welcome db "Welcome to my OS.", 0
 
 times 510-($-start) db 0
 dw 0xaa55 ; Tells the bios that it is bootable. 55aa in little endian(x86).
