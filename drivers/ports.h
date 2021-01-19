d60fe77b4f157f0ba7971e1155bdec8d3504bb62
Shachaf Zohar <shafzhr@gmail.com>
2021-01-19 10:33:33 +0200

    header guard

--- drivers/ports.h
+++ drivers/ports.h
@@ -1,4 +1,9 @@
+#ifndef H_PORTS
+#define H_PORTS
+
 unsigned char port_in(unsigned short port);
 void port_out(unsigned short port, unsigned char data);
 unsigned short word_port_in(unsigned short port);
 void word_port_out(unsigned short port, unsigned short data);
+
+#endif
