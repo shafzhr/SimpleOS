4ce8c08feb87d15a52da7ba211e5c18d905953f5
Shachaf Zohar <shafzhr@gmail.com>
2021-02-28 19:34:27 +0200

    Added pci basic interface

--- cpu/pci.c
+++ cpu/pci.c
@@ -1,38 +1,84 @@
+#include "pci.h"
 #include "../drivers/ports.h"
 #include <stdint.h>
 
 
-/**
- * @brief Reads 16-bit from the configuration space.
- *        https://wiki.osdev.org/PCI#Configuration_Space_Access_Mechanism_.231
- * 
- * @param bus Bus number
- * @param slot Device number
- * @param func Function number
- * @param offset Register offset - has to point to consecutive DWORDs -> bits 0 ant 1 are always 0.
- * @return uint16_t 
- */
-uint16_t pciRead(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset)
+
+static uint32_t pciGetAddress(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset)
 {
-    uint32_t address;
     uint32_t lbus  = (uint32_t)bus;
     uint32_t lslot = (uint32_t)slot;
     uint32_t lfunc = (uint32_t)func;
-    uint16_t tmp = 0;
- 
-    /* create configuration address as per Figure 1 */
-    address = (uint32_t)((lbus << 16) | (lslot << 11) |
-              (lfunc << 8) | (offset & 0xfc) | ((uint32_t)0x80000000));
- 
-    /* write out the address */
-    outl(0xCF8, address);
-    /* read in the data */
-    /* (offset & 2) * 8) = 0 will choose the first word of the 32 bits register */
-    tmp = (uint16_t)((inl(0xCFC) >> ((offset & 2) * 8)) & 0xffff);
-    return tmp;
+
+    return (lbus << 16) | (lslot << 11) | (lfunc << 8) | 0x80000000;   
 }
 
-uint16_t pciWrite(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint32_t data)
+/**
+ * @brief Reads 32-bit from the configuration space.
+ *        https://wiki.osdev.org/PCI#Configuration_Space_Access_Mechanism_.231
+ * 
+ * @param address device's configuration address
+ * @param offset Register offset - has to point to consecutive DWORDs -> bits 0 ant 1 are always 0.
+ * @return uint32_t read data
+ */
+uint32_t pciRead(uint32_t address, uint8_t offset)
 {
+    /* write out the address */
+    outl(PCI_CONFIG_ADDR_PORT, address | (offset | 0xFC));
+    /* read in the data */
+    return inl(PCI_CONFIG_DATA_PORT);
+}
+
+void pciWrite(uint32_t address, uint8_t offset, uint32_t data)
+{
+    outl(PCI_CONFIG_ADDR_PORT, address | (offset | 0xFC));
+    outl(PCI_CONFIG_DATA_PORT, data);
+}
+
+uint16_t pciGetVendorId(uint32_t address)
+{
+    uint32_t vendor_id = pciRead(address, 0) & 0xFFFF;
+    return vendor_id;
+}
+
+uint16_t pciGetDeviceId(uint32_t address)
+{
+    uint32_t device_id = (pciRead(address, 0) >> 16) & 0xFFFF;
+    return device_id;
+}
+
+/**
+ * @brief gets device by vendor id and device id
+ *        TODO: - Add support for multiple devices with the same vendor id and device id.
+ *              - Add support for functions.
+ * 
+ * @param vendor_id vendor id
+ * @param device_id device id
+ * @return pci_device_t
+ */
+pci_device_t pciGetDevice(uint16_t vendor_id, uint16_t device_id)
+{
+    for (uint8_t bus = 0; bus < 8; ++bus)
+    {
+        for (uint8_t slot = 0; slot < 32; ++slot)
+        {
+            uint32_t address = pciGetAddress(bus, slot, 0, 0);
+            uint16_t v_id = pciGetVendorId(address);
+            if (v_id == PCI_NON_EXISTENT)
+            {
+                continue;
+            }
+            uint16_t d_id = pciGetDeviceId(address);
+            if (vendor_id == v_id && device_id == d_id)
+            {
+                pci_device_t device = { 0 };
+                device.address = address;
+                device.vendor_id = vendor_id;
+                device.device_id = device_id;
+                device.bar0 = pciRead(address, PCI_BAR0);
+            }
+        }
+        
+    }
     
 }
--- cpu/pci.h
+++ cpu/pci.h
@@ -3,6 +3,23 @@
 
 #include <stdint.h>
 
-uint16_t pciRead(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+#define PCI_CONFIG_ADDR_PORT 0xCF8
+#define PCI_CONFIG_DATA_PORT 0xCFC
+#define PCI_NON_EXISTENT 0xFFFF
+
+#define PCI_BAR0 0x10
+
+typedef struct
+{
+    uint32_t address;
+    uint32_t device_id, vendor_id;
+    uint32_t bar0, bar1, bar2, bar3, bar4, bar5;
+
+} pci_device_t;
+
+
+uint32_t pciRead(uint32_t address, uint8_t offset);
+void pciWrite(uint32_t address, uint8_t offset, uint32_t data);
+pci_device_t pciGetDevice(uint16_t vendor_id, uint16_t device_id);
 
 #endif
